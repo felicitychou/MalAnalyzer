f6d7198d0ba0c1b5c28a749773076b3f2cac9778
felicitychou <felicitychou@hotmail.com>
2017-02-22 22:00:40 +0800

    add ssdeep

--- basic_analyze.py
+++ basic_analyze.py
@@ -9,7 +9,7 @@ import magic
 import subprocess
 import hashlib
 import binascii
-
+import ssdeep
 import pefile
 
 
@@ -21,17 +21,19 @@ class BasicAnalyzer(object):
         self.filepath = filepath
         self.get_file_basic_info()
 
-    def get_file_basic_info(self):
+    def run(self):
         '''
         return {filename,filetype,filesize(Byte)}
         '''
         self.filename = os.path.basename(self.filepath)
         self.filetype = magic.from_file(self.filepath)
         self.filesize = int(os.path.getsize(self.filepath))
-        self.md5 = self.hash_file('md5')
-        self.sha1 = self.hash_file('sha1')
+        self.hash = {"md5":self.hash_file('md5'),
+                     "sha256":self.hash_file('sha256'),
+                     "crc32":self.get_crc32(),
+                     "ssdeep":self.get_ssdeep()}
 
-    # get pe info
+    # get pe info ？？？
     def get_pe_info(self):
         pe = pefile.PE(self.filepath)
 
@@ -45,12 +47,12 @@ class BasicAnalyzer(object):
                 print '\t', hex(imp.address), imp.name
         pass
 
-    # get elf info
+    # get elf info ？？？
     def get_elf_info(self):
         pass
 
 
-    # get strings unicode and ascii
+    # get strings unicode and ascii ？？？
     def get_strings(self):
         # windows
         # strings.exe https://technet.microsoft.com/en-us/sysinternals/bb897439.aspx
@@ -60,9 +62,8 @@ class BasicAnalyzer(object):
         unicode_strings = subprocess.check_output(["strings", "-a", "-el", self.filepath])
         return ascii_strings, unicode_strings
 
-    # hash
+    # get hash ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
     def hash_file(self, hash_type):
-        # file hash cal hash_type('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
         hash_handle = getattr(hashlib, hash_type)()
         with open(self.filepath, 'rb') as file:
             hash_handle.update(file.read())
@@ -72,3 +73,7 @@ class BasicAnalyzer(object):
     def get_crc32(self):
         with open(self.filepath, 'rb') as file:
             return '%x' % (binascii.crc32(file.read()) & 0xffffffff)
+
+    # get ssdeep
+    def get_ssdeep(self):
+        return ssdeep.hash_from_file(self.filepath)
